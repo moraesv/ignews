a5143b621a452eb89a3cb4c9eb42f0b30f63dc95
Vinicius de Moraes <vimoraescarvalho@gmail.com>
2021-04-05 19:12:10 -0300

    Create app structure and styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News abou the <span>React</span> world.
+          </h1>
+
+          <p>
+            get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
+}
