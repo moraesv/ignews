a3fe6f78ae290ccb348279364458924a694f64bb
Vinicius de Moraes <vimoraescarvalho@gmail.com>
2021-04-20 20:09:59 -0300

    Create more events to stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Readable } from 'stream';
 import Stripe from 'stripe';
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscriptions';
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -18,7 +19,11 @@ export const config = {
   },
 };
 
-const relevantEvents = new Set(['checkout.session.completed']);
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -40,7 +45,39 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log('evento recebido', event);
+      try {
+        switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id.toString(),
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
+          case 'checkout.session.completed':
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            );
+
+            break;
+
+          default:
+            throw new Error('Unhandled event');
+        }
+      } catch (error) {
+        console.log(error);
+
+        return res.json({ error: 'Webhook handler failed.' });
+      }
     }
 
     res.json({ received: true });
